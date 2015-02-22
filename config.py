e0039270f0553af736921ec144f20c72ceb2a8eb
Harshay Shah <harshay.rshah@gmail.com>
2015-02-21 20:14:06 -0600

    Procfile done and config semi setup

---
+++ config.py
@@ -0,0 +1,24 @@
+import os 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'devDB.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'mainDB.sqlite')
+
+config = {
+    'default': DevelopmentConfig,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
