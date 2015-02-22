bc644919d7897a7ff93ce4b7449ab4f025134db6
Harshay Shah <harshay.rshah@gmail.com>
2015-02-22 01:09:13 -0600

    auth blueprint - views, forms

---
+++ app/auth/forms.py
@@ -0,0 +1,38 @@
+from flask import flash 
+from flask.ext.wtf import Form 
+from wtforms import TextField, SubmitField, ValidationError, PasswordField, BooleanField, validators
+from ..models import User
+# from .. import db, login_manager 
+
+def username_exist(form, field):
+    l = User.query.filter_by(username=form.username.data).all()
+    if l != 0:
+        raise ValidationError("Username already exists. Try something else.")
+
+def username_dne(form, field):
+    l = User.query.filter_by(username=form.username.data).all()
+    if l == 0:
+        raise ValidationError("Username does not exist. Try again.")
+
+def password_wrong(form, field):
+    user = User.query.filter_by(username=form.username.data).first()
+    if user == None:
+        raise ValidationError("Incorrect password and username combination")
+    if not user.check_password(form.password.data):
+        raise ValidationError("Incorrect password and username combination")
+
+class LoginForm(Form):
+    username = TextField("Username", [validators.DataRequired(), username_dne])
+    password = PasswordField("Password", [validators.DataRequired(), password_wrong])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign in")
+
+class RegisterForm(Form):
+    username = TextField("Username", [validators.Length(5,25), username_exist])
+    password = PasswordField("Password",[validators.length(7, 25), validators.EqualTo('retype_password', message = "Passwords must match")])
+    retype_password = PasswordField("Retype Password", [validators.DataRequired()])
+    submit = SubmitField("Create Account")
+
+        
+
+
---
+++ app/auth/views.py
@@ -0,0 +1,47 @@
+from flask import render_template, session, g, flash, redirect, url_for, request 
+from flask.ext.login import login_user, login_required, logout_user
+
+from . import auth 
+from forms import LoginForm, RegisterForm 
+from .. import db, login_manager
+from ..models import User
+
+
+@auth.route('/login', methods=["GET","POST"])
+def login():
+    form = LoginForm 
+    if request.method == "GET":
+        return "Login page"
+    
+    if form.validate_on_submit():
+        user = User.get_username(form.username.data)
+        login_user(user, form.remember_me.data)
+        return redirect('/')
+
+    return "Login page"
+
+@auth.route("/register", methods = ["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if request.method == "GET":
+        return "Register Page"
+    
+    elif request.method == "POST":
+        if form.validate_on_submit():
+            newuser = User(form.username.data, form.password.data)
+            db.session.add(newuser)
+            # no need to commit, db commit on teardown is true
+            login_user(newuser, False)
+            return redirect('/')
+        
+        return "Register Page"
+  
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out")
+
+    
