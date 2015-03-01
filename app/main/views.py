37e4affe9eb8d07bc864613f6a76e8e989533b7a
Harshay Shah <harshay.rshah@gmail.com>
2015-02-28 22:54:51 -0600

    additional paramters for rendering template

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from . import main
 from .forms import TodoForm
 from .. import db 
 from ..models import User, Todo
+from collections import Counter
 
 @main.app_errorhandler(404)
 def page_not_found(e):
@@ -24,14 +25,20 @@ def index():
 @login_required
 def profile():
     form = TodoForm()
+
+    user_todo_all = current_user.todo.all()
+    user_todo, user_todo_done = filter(lambda x: x.done == False, user_todo_all), filter(lambda x: x.done, user_todo_all) 
+    user_todo_hashtags = Counter(list(map(lambda x: x.hashtag, user_todo)))
+    del user_todo_hashtags[""]
+    
     if request.method == "POST" and form.validate_on_submit():
         parsed_todo = parse_todo(form.todo.data)
         newtodo = Todo(parsed_todo[0], parsed_todo[1]).save()
-        user_todo = reversed(current_user.todo.filter_by(done = False).all())
         form.todo.data = ""
-        return render_template("profile.html", form = form, td = user_todo)
-    user_todo = reversed(current_user.todo.filter_by(done = False).all())
-    return render_template("profile.html", form = form, td = user_todo)
+        url = url_for('main.profile')
+        return redirect(url)
+    
+    return render_template("profile.html", form = form, td = user_todo, hsh = user_todo_hashtags, sz = len(user_todo))
 
 def parse_todo(todo):
     todo = todo.strip().split()
@@ -39,5 +46,9 @@ def parse_todo(todo):
     tags = "".join(filter(lambda x: x.startswith("#"), todo))
     return words, tags
 
+@main.route('/done', methods=["POST"])
+@login_required 
+def done():
+    pass
 
 
