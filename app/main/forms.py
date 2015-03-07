8179e613d0339a231c6a1b6472d786a39f09ba81
Harshay Shah <harshay.rshah@gmail.com>
2015-03-07 11:40:14 -0600

    todo validation bug fixed

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,11 +3,12 @@ from flask.ext.wtf import Form
 from wtforms import TextField, SubmitField, ValidationError, BooleanField, validators
 from ..models import Todo 
 import views
+from flask.ext.login import current_user
 
 def todo_exists(form, field):
     todo = views.parse_todo(form.todo.data)[0]
     print todo
-    todo_db = Todo.query.filter_by(description = todo).first()
+    todo_db = current_user.todo.filter_by(description = todo).first()
     if todo_db != None:
         raise ValidationError("Task already exists")
 
